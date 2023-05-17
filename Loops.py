e02c68e75764ba2959906c15c4b0de2513d1f65d
Shruti23052003 <126007104+Shruti23052003@users.noreply.github.com>
2023-05-17 23:07:55 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,20 @@
+# To print all positive numbers in a range
+
+list1 = [12,-7,5,64,-14]
+
+list2 = [12,14,-95,3]
+
+print("For list1")
+
+for number in list1:
+    if number >= 0:
+      print(number)
+      
+      
+
+print("for list2") 
+
+for number in list2:
+    if number >= 0:
+        print(number)
+      
